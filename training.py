81f0c21199f1a50ec25f24c3f238a356d479fbe5
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-11-15 08:12:20 -0500

    Create training.py

---
+++ training.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import cv2
+from PIL import Image
+import numpy as np
+import cv2
+# Load the model.
+model = YOLO('yolov8n.pt')
+
+# Training.
+results = model.train(
+    data='/PATH/TO/YOUR/DIRECTORY/data.yaml', # this is the path to your training data using roboflow to annotate 
+    imgsz=640,                                # export it as YOLOv7 and you will get a data.yaml
+    epochs=100,
+    augment=True,
+    batch=16,
+    name='ChessCornerDetection',
+    project='/PATH/TO/YOUR/DIRECTORY')
