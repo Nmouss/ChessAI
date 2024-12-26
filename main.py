dc6c5e7c78a7da870f92330da31461d0c9608946
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-12-26 12:46:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ChessAI as Chess
 from ultralytics import YOLO
+
 print("type 'w', 'b' or 'white', 'black'") # of course this will be replaced with buttons when i can get glasses that have cameras
 turn = input("White or Black's turn to go? ").lower()
 if turn == 'w' or turn == 'white':
